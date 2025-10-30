e7786f6709687391157804a7ade78dfbdb25adfe
David Hugh-Jones <davidhughjones@gmail.com>
2025-10-30 21:36:20 +0000

    Improve docs

--- R/mlx-methods.R
+++ R/mlx-methods.R
@@ -8,14 +8,17 @@
 #' - [`%*%`](%*%.mlx) for matrix multiplication
 #' - [`[`]([.mlx) and [`[<-`]([<-.mlx) for extraction and assignment
 #' - [`Ops`](Ops.mlx) and [`Math`](Math.mlx) for elementwise arithmetic and math
-#' - [`Summary`](Summary.mlx) for reductions such as `sum()` and `max()`
+#' - [`Summary`](Summary.mlx) for reductions such as `sum()` and `max()`;
+#'   also [`mean()`](mean.mlx), [`length()`](length.mlx) and [`all.equal()`](all.equal.mlx).
+#' - [`diag()`](diag.mlx), [`dim()`](dim.mlx) and [`dim<-`](dim<-.mlx)
 #' - [`as.matrix()`](as.matrix.mlx), [`as.array()`](as.array.mlx), and [`as.vector()`](as.vector.mlx) for conversion back to base R
-#' - [`cbind()`](cbind.mlx) and [`rbind()`](rbind.mlx) for binding arrays along rows or columns
+#' - [`cbind()`](cbind.mlx) and [`rbind()`](rbind.mlx) for binding arrays along rows or columns;
+#'   there is also an [abind()] function modelled on `abind::abind()`.
 #' - [`rowMeans()`](rowMeans.mlx), [`colMeans()`](colMeans.mlx), [`rowSums()`](rowSums.mlx), and [`colSums()`](colSums.mlx) for axis-wise summaries
 #' - [`aperm()`](aperm.mlx), [`t()`](t.mlx), and [`dim<-`](\`dim<-.mlx\`) for shape manipulation
 #' - [`kronecker()`](kronecker), [`outer()`](outer.mlx), [`crossprod()`](crossprod), and [`tcrossprod()`](tcrossprod) for linear algebra helpers
 #' - [`fft()`](fft.mlx), [`chol()`](chol.mlx), [`chol2inv()`](chol2inv), and [`solve()`](solve.mlx) for numerical routines
-#'
+#' - [`is.finite()`](is.finite.mlx), [`is.infinite()`](is.infinite.mlx) and [`is.nan()`](is.nan.mlx)
 #' @seealso [as_mlx()]
 #'
 #' @name mlx-methods
